39b2ba3bbeabc9b0bf1467edd7e3583e3ff29059
Angel Ramirez <angelra@gmail.com>
2024-10-25 18:33:10 -0700

    Player selects number of bots and size of bag to start with

--- src/game.py
+++ src/game.py
@@ -11,10 +11,12 @@ class Game():
         self.bot_players = []
         self.minimum_bet = 5
         self.seats = [[]] * 5
+        self.is_active = False
     
     def _create_human_player(self):
         player = Player()
         self.human_player = player
+        return player
         
     def _create_bot_players(self, bots):
         if 0 < bots < 5:
@@ -38,13 +40,15 @@ class Game():
             if seat_number != reserved_seat and self.seats[seat_number] == []:
                 self.seats[seat_number] = self.bot_players[bots_remaining - 1]
                 bots_remaining -= 1
-        
+    
     def start_new_game(self, bots = 0):
         self._create_human_player()
         self._create_dealer()
-        self._create_bot_players(bots)
+        self.game_start_greeting()
+        self._create_bot_players(self.number_of_bots)
         self._assign_seating()
         self.shuffle_deck()
+        self.is_active = True
     
     def shuffle_deck(self):
         deck = Deck()
@@ -54,6 +58,27 @@ class Game():
             card = deck.game_deck[card_index]
             self.deck.append(card)
             del deck.game_deck[card_index]
+            
+    def game_start_greeting(self):
+        print(f"current player: {self.human_player}")
+        print(f"Welcome to the Blackjack table!\nThis table has minimum bets of {self.minimum_bet}.\nHow much would you like to cash in?")
+        amount = float(input())
+        self.human_player.cash_money = amount
+        print(f"Deep pockets! You now have {amount} worth of chips.")
+        print("Would you like to 1v1 the house or have some company? How many bot players would you like to play with?")
+        number_of_bots = int(input())
+        while (0 > number_of_bots or number_of_bots > 4):
+            print("Please select a number from 0 to 4:")
+            number_of_bots = int(input())
+        if (number_of_bots == 1):
+            print("OKAY! I see you want to 1v1. Good luck!")
+        else:
+            print("Excellent! Good luck, all!")
+        self.number_of_bots = number_of_bots
+        
+    def place_bets(self):
+        print("Place your bets!")
+        current_bet = float(input())
     
 
 class Player():
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from game import *
 def main():
     game = Game()
-    game.start_new_game()    
-    print(game.deck[0].name)
+    game.start_new_game()
+    while (game.is_active):
+        game.place_bets()
 main()
